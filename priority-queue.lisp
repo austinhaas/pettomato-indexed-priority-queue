7c69778a2d2503d6d092a7235bb25de795e5b6be
Austin Haas <austin@pettomato.com>
2011-12-31 19:05:44 -0800

    Refactored.

--- priority-queue.lisp
+++ priority-queue.lisp
@@ -1,6 +1,6 @@
 (in-package #:priority-queue)
 
-;;;; Conditions
+(defconstant +not-in-queue+ -1)
 
 (define-condition empty-queue-error (error)
   ()
@@ -10,9 +10,48 @@
   ()
   (:documentation "Signaled when an operation depends on an item that was not found in the queue."))
 
-;;;; Basic Operations on Queues
+(declaim (inline parent))
+(defun parent (i) (declare (fixnum i)) (floor (- i 1) 2))
+(declaim (inline left))
+(defun left (i) (declare (fixnum i)) (the fixnum (+ 1 i i)))
+(declaim (inline right))
+(defun right (i) (declare (fixnum i)) (the fixnum (+ 2 i i)))
+(declaim (inline exchange))
+(defun exchange (heap a b set-index-fn)
+  (declare (fixnum a b) (function set-index-fn))
+  (rotatef (aref heap a) (aref heap b))
+  (funcall set-index-fn (aref heap a) a)
+  (funcall set-index-fn (aref heap b) b))
 
-(defconstant +not-in-queue+ -1)
+(defun heapify (heap i compare-fn set-index-fn)
+  "Assume that the children of i are heaps, but that heap[i] may be
+worse than its children. If it is, move heap[i] down where it
+belongs. [Page 130 CL&R 2nd ed.]."
+  (declare (array heap) (fixnum i) (function compare-fn) (function set-index-fn))
+  (loop with N = (length heap)
+        with item = (aref heap i)
+        for l = (left i)
+        for r = (right i)
+        for best = i
+        do (when (and (< l N) (funcall compare-fn (aref heap l) (aref heap i)))
+             (setf best l))
+           (when (and (< r N) (funcall compare-fn (aref heap r) (aref heap best)))
+             (setf best r))
+           (cond ((/= best i)
+                  (rotatef (aref heap i) (aref heap best))
+                  (funcall set-index-fn (aref heap i) i)
+                  (setf i best))
+                 (t
+                  (funcall set-index-fn item i)
+                  (return)))))
+
+(defun improve-key (heap i compare-fn set-index-fn)
+  (declare (array heap) (fixnum i) (function compare-fn) (function set-index-fn))
+  (let ((item (aref heap i)))
+    (loop while (and (> i 0)
+                     (funcall compare-fn item (aref heap (parent i))))
+          do (exchange heap i (parent i) set-index-fn)
+             (setf i (parent i)))))
 
 (defstruct q
   (compare-fn nil :type function)
@@ -20,7 +59,7 @@
   (get-index-fn nil :type function)
   (items nil :type array))
 
-(defun make-empty-queue (compare-fn set-index-fn get-index-fn &key (size 100) (element-type t))
+(defun make-empty-queue (compare-fn set-index-fn get-index-fn &key (size 100))
   "Return a new empty queue.
 
 compare-fn is a function that takes two arguments and returns a true
@@ -41,7 +80,7 @@ index that was associated with it via set-index-fn.
   (make-q :compare-fn compare-fn
           :set-index-fn set-index-fn
           :get-index-fn get-index-fn
-          :items (make-array size :element-type element-type :fill-pointer 0 :adjustable t)))
+          :items (make-array size :fill-pointer 0 :adjustable t)))
 
 (defun queue-empty-p (q)
   "Are there no items in the queue?"
@@ -56,17 +95,30 @@ empty-queue-error if the queue is empty."
 
 (defun queue-pop (q)
   "Remove the element from the front of the queue and return
-it. Signals empty-queue-error if the queue is empty."
+it. Signals empty-queue-error if the queue is empty. [Page 139 CL&R
+2nd ed.]."
   (if (queue-empty-p q)
       (error 'empty-queue-error)
-      (let ((min (heap-extract-min (q-items q) (q-compare-fn q) (q-set-index-fn q))))
-        (funcall (q-set-index-fn q) min +not-in-queue+)
+      (let* ((heap (q-items q))
+             (set-index-fn (q-set-index-fn q))
+             (min (aref heap 0)))
+        (when (> (length heap) 1)
+          (setf (aref heap 0) (aref heap (1- (length heap))))
+          (funcall set-index-fn (aref heap 0) 0))
+        (decf (fill-pointer heap))
+        (heapify heap 0 (q-compare-fn q) set-index-fn)
+        (funcall set-index-fn min +not-in-queue+)
         min)))
 
 (defun queue-insert (q item)
   "Insert the item by priority according to the compare
-function. Returns the queue."
-  (heap-insert (q-items q) item (q-compare-fn q) (q-set-index-fn q))
+function. Returns the queue. [Page 140 CL&R 2nd ed.]."
+  (let ((heap (q-items q))
+        (compare-fn (q-compare-fn q))
+        (set-index-fn (q-set-index-fn q)))
+    (let ((i (vector-push-extend item heap)))
+      (funcall set-index-fn item i)
+      (improve-key heap i compare-fn set-index-fn)))
   q)
 
 (defun queue-replace (q old new)
@@ -87,7 +139,7 @@ item-not-found-error if the old item wasn't found."
     (funcall set-index-fn new index)
     (setf (aref items index) new)
     (if (funcall compare-fn new old)
-        (heap-improve-key items index compare-fn set-index-fn)
+        (improve-key items index compare-fn set-index-fn)
         (heapify items index compare-fn set-index-fn)))
   q)
 
@@ -104,8 +156,8 @@ wasn't found."
               (= index +not-in-queue+))
       (error 'item-not-found-error))
     (if (and (> index 0)
-             (funcall compare-fn item (aref items (heap-parent index))))
-        (heap-improve-key items index compare-fn (q-set-index-fn q))
+             (funcall compare-fn item (aref items (parent index))))
+        (improve-key items index compare-fn (q-set-index-fn q))
         (heapify items index compare-fn (q-set-index-fn q))))
   q)
 
@@ -114,100 +166,29 @@ wasn't found."
 item-not-found-error if the item wasn't found."
   (when (queue-empty-p q)
     (error 'empty-queue-error))
-  (let ((index (funcall (q-get-index-fn q) item)))
-    (declare (fixnum index))
-    (when (or (null index)
-              (= index +not-in-queue+))
-      (error 'item-not-found-error))
-    (heap-delete (q-items q) index (q-compare-fn q) (q-set-index-fn q)))
-    (funcall (q-set-index-fn q) item +not-in-queue+)
+  (let ((heap (q-items q))
+        (compare-fn (q-compare-fn q))
+        (get-index-fn (q-get-index-fn q))
+        (set-index-fn (q-set-index-fn q)))
+   (let ((index (funcall get-index-fn item)))
+     (declare (fixnum index))
+     (when (or (null index)
+               (= index +not-in-queue+))
+       (error 'item-not-found-error))
+     (let ((L (length heap)))
+       (cond ((or (= L 1)
+                  (= index (1- L)))
+              ;; There is only one item in the heap or the item we are
+              ;; after is positioned last in the heap.
+              (decf (fill-pointer heap)))
+             (t
+              (let ((last (aref heap (1- L))))
+                (setf (aref heap index) last)
+                (funcall set-index-fn last index)
+                (decf (fill-pointer heap))
+                (if (and (> index 0)
+                         (funcall compare-fn last (aref heap (parent index))))
+                    (improve-key heap index compare-fn set-index-fn)
+                    (heapify heap index compare-fn set-index-fn))))))))
+  (funcall (q-set-index-fn q) item +not-in-queue+)
   q)
-
-;;;; The Heap Implementation of Priority Queues
-
-(declaim (inline heap-parent))
-(defun heap-parent (i) (declare (fixnum i)) (floor (- i 1) 2))
-(declaim (inline heap-left))
-(defun heap-left (i) (declare (fixnum i)) (the fixnum (+ 1 i i)))
-(declaim (inline heap-right))
-(defun heap-right (i) (declare (fixnum i)) (the fixnum (+ 2 i i)))
-(declaim (inline exchange))
-(defun exchange (heap a b set-index-fn)
-  (declare (fixnum a b) (function set-index-fn))
-  (rotatef (aref heap a) (aref heap b))
-  (funcall set-index-fn (aref heap a) a)
-  (funcall set-index-fn (aref heap b) b))
-
-(defun heapify (heap i compare-fn set-index-fn)
-  "Assume that the children of i are heaps, but that heap[i] may be
-worse than its children. If it is, move heap[i] down where it
-belongs. [Page 130 CL&R 2nd ed.]."
-  (declare (array heap) (fixnum i) (function compare-fn) (function set-index-fn))
-  (loop with N = (length heap)
-        with item = (aref heap i)
-        for l = (heap-left i)
-        for r = (heap-right i)
-        for best = i
-        do (when (and (< l N)
-                      (funcall compare-fn (aref heap l) (aref heap i)))
-             (setf best l))
-           (when (and (< r N)
-                      (funcall compare-fn (aref heap r) (aref heap best)))
-             (setf best r))
-           (cond ((/= best i)
-                  (rotatef (aref heap i) (aref heap best))
-                  (funcall set-index-fn (aref heap i) i)
-                  (setf i best))
-                 (t
-                  (funcall set-index-fn item i)
-                  (return)))))
-
-(defun heap-extract-min (heap compare-fn set-index-fn)
-  "Pop the best (lowest valued) item off the heap. [Page 139 CL&R 2nd ed.]."
-  (declare (array heap) (function compare-fn) (function set-index-fn))
-  (assert (> (length heap) 0) nil "heap underflow")
-  (let ((min (aref heap 0)))
-    (funcall set-index-fn min +not-in-queue+)
-    (when (> (length heap) 1)
-      (setf (aref heap 0) (aref heap (1- (length heap))))
-      (funcall set-index-fn (aref heap 0) 0))
-    (decf (fill-pointer heap))
-    (heapify heap 0 compare-fn set-index-fn)
-    min))
-
-(defun heap-improve-key (heap i compare-fn set-index-fn)
-  (declare (array heap) (fixnum i) (function compare-fn) (function set-index-fn))
-  (let ((item (aref heap i)))
-    (loop while (and (> i 0)
-                     (funcall compare-fn item (aref heap (heap-parent i))))
-          do (exchange heap i (heap-parent i) set-index-fn)
-             (setf i (heap-parent i)))))
-
-(defun heap-insert (heap item compare-fn set-index-fn)
-  "Put an item into a heap. [Page 140 CL&R 2nd ed.]."
-  (declare (array heap) (function compare-fn) (function set-index-fn))
-  (let ((i (vector-push-extend item heap)))
-    (funcall set-index-fn item i)
-    (heap-improve-key heap i compare-fn set-index-fn)))
-
-(defun heap-delete (heap i compare-fn set-index-fn)
-  "Delete the item at position i."
-  (declare (array heap) (fixnum i) (function compare-fn) (function set-index-fn))
-  (let ((L (length heap)))
-    (assert (> L 0) nil "heap underflow")
-    (assert (and (>= i 0)
-                 (< i L)) nil "index out of range")
-    (cond ((or (= L 1)
-               (= i (1- L)))
-           ;; There is only one item in the heap or the item we are
-           ;; after is positioned last in the heap.
-           (decf (fill-pointer heap)))
-          (t
-           (let ((last (aref heap (1- L))))
-             (setf (aref heap i) last)
-             (funcall set-index-fn last i)
-             (decf (fill-pointer heap))
-             (if (and (> i 0)
-                      (funcall compare-fn last (aref heap (heap-parent i))))
-                 (heap-improve-key heap i compare-fn set-index-fn)
-                 (heapify heap i compare-fn set-index-fn)))))))
